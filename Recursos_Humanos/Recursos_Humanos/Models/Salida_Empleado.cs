27664df994c3d5861ae420245e8098414447cda8
daniel_lopez1805@outlook.com <daniel_lopez1805@outlook.com>
2019-12-04 02:12:05 -0400

    codigos del controlador y del modelo de Salida_Empleado

--- Recursos_Humanos/Recursos_Humanos/Models/Salida_Empleado.cs
+++ Recursos_Humanos/Recursos_Humanos/Models/Salida_Empleado.cs
@@ -7,5 +7,20 @@ namespace Recursos_Humanos.Models
 {
 	public class Salida_Empleado
 	{
-	}
+        [Key]
+        public int Id_Salida { get; set; }
+        public int Id_Empleado { get; set; }
+
+        [Required]
+        public String Tipo_Salida { get; set; } //(Renuncia, Despido, Desahucio)
+
+        [Required]
+        public String Motivo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Salida { get; set; }
+
+        public Empleado Empleado { get; set; }
+    }
 }
