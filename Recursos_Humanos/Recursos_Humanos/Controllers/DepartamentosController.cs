3a99252ee95f84d20a65b1b9e25e77007bd7b516
edicksom tejeda <ediiicksom@gmail.com>
2019-12-03 23:04:52 -0400

    modelos a trabajar y codigos del cotrolador y modelo de departamentos

--- Recursos_Humanos/Recursos_Humanos/Controllers/DepartamentosController.cs
+++ Recursos_Humanos/Recursos_Humanos/Controllers/DepartamentosController.cs
@@ -8,10 +8,116 @@ namespace Recursos_Humanos.Controllers
 {
     public class DepartamentosController : Controller
     {
-        // GET: Departamentos
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+		private GestionContext db = new GestionContext();
+
+		// GET: Departamentos
+		public ActionResult Index()
+		{
+			return View(db.Departamentos.ToList());
+		}
+
+		// GET: Departamentos/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Departamento departamento = db.Departamentos.Find(id);
+			if (departamento == null)
+			{
+				return HttpNotFound();
+			}
+			return View(departamento);
+		}
+
+		// GET: Departamentos/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Departamentos/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "Id_Depto,Codigo_Depto,Nombre_Depto")] Departamento departamento)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Departamentos.Add(departamento);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(departamento);
+		}
+
+		// GET: Departamentos/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Departamento departamento = db.Departamentos.Find(id);
+			if (departamento == null)
+			{
+				return HttpNotFound();
+			}
+			return View(departamento);
+		}
+
+		// POST: Departamentos/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "Id_Depto,Codigo_Depto,Nombre_Depto")] Departamento departamento)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(departamento).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(departamento);
+		}
+
+		// GET: Departamentos/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Departamento departamento = db.Departamentos.Find(id);
+			if (departamento == null)
+			{
+				return HttpNotFound();
+			}
+			return View(departamento);
+		}
+
+		// POST: Departamentos/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Departamento departamento = db.Departamentos.Find(id);
+			db.Departamentos.Remove(departamento);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
---
+++ Recursos_Humanos/Recursos_Humanos/Models/Departamentos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recursos_Humanos.Models
+{
+	public class Departamentos
+	{
+		[Key]
+		public int Id_Depto { get; set; }
+
+		[Required]
+		[StringLength(4)]
+		public String Codigo_Depto { get; set; }
+
+		[StringLength(50)]
+		public String Nombre_Depto { get; set; }
+
+		//public Empleado Empleado { get; set; }
+		public List<Empleado> Empleados { get; set; }
+	}
+}
