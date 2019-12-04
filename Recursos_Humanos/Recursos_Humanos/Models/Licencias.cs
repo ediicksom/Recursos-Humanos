13265618d1b4e4d217bc57bf480ae25feb87054e
edicksom tejeda <ediiicksom@gmail.com>
2019-12-04 00:03:38 -0400

    codigos del controlador y modelo de licencias

--- Recursos_Humanos/Recursos_Humanos/Models/Licencias.cs
+++ Recursos_Humanos/Recursos_Humanos/Models/Licencias.cs
@@ -7,5 +7,24 @@ namespace Recursos_Humanos.Models
 {
 	public class Licencias
 	{
+		[Key]
+		public int Id_licencia { get; set; }
+
+
+		public int Id_Empleado { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime Inicio_Permiso { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime Fin_Permiso { get; set; }
+
+		[Required]
+		public String Motivo { get; set; }
+		public String Comentario { get; set; }
+
+		public Empleado Empleado { get; set; }
 	}
 }
